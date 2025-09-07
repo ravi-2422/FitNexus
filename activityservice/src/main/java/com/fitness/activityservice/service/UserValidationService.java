1897ca588318dd441e6b8b2ae0f6e98d28aa02f7
Ravi Ranjan Kumar <ravirkumar2422@gmail.com>
2025-09-08 02:03:53 +0530

    Create microservices for user and activity service

---
+++ activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -0,0 +1,35 @@
+package com.fitness.activityservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserValidationService {
+    private final WebClient userServiceWebClient;
+
+    public boolean validateUser(String userId) {
+        log.info("Calling User Validation API for userId: {}", userId);
+        try{
+            Boolean isValid = userServiceWebClient.get()
+                    .uri("/api/users/{userId}/validate", userId)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block();
+            log.info("User validation response for {}: {}", userId, isValid);
+            return Boolean.TRUE.equals(isValid);
+
+        } catch (WebClientResponseException e) {
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND)
+                throw new RuntimeException("User Not Found: " + userId);
+            else if (e.getStatusCode() == HttpStatus.BAD_REQUEST)
+                throw new RuntimeException("Invalid Request: " + userId);
+        }
+        return false;
+    }
+}

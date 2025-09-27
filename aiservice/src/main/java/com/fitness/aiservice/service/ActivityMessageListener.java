4668ad5e96066ff5e8246bbe20a46e80d110cff9
Ravi Ranjan Kumar <ravirkumar2422@gmail.com>
2025-09-27 16:53:47 +0530

    Added the recommendations controllers

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -12,13 +12,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class ActivityMessageListener {
-   // private final ActivityAIService aiService;
+    private final ActivityAIService aiService;
     private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getId());
-//        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
 //        Recommendation recommendation = aiService.generateRecommendation(activity);
 //        recommendationRepository.save(recommendation);
     }
